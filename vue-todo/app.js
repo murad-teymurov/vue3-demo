bb0160a2ae1a4b1b90c9f46591b347ce17c8489b
Murad Teymurov <murad.teymurov@sinam.net>
2023-12-04 14:12:13 +0400

    practice - 4

--- vue-todo/app.js
+++ vue-todo/app.js
@@ -4,28 +4,33 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: "https://www.instagram.com/muradtyv/",
             showBooks: true,
-            title: "The Final Empire",
-            author: "brandon Sanderson",
-            age: 45,
-            x:0,
-            y:0
+            books: [
+                {
+                    title: "name of the wind",
+                    author: "patrick rothfuss",
+                    img: "assets/1.jpg",
+                    isFav: true
+                },{
+                    title: "the way of kings",
+                    author: "brandom sanderson",
+                    img: "assets/2.jpg",
+                    isFav: false
+                },{
+                    title: "the final empire",
+                    author: "brandon sanderson",
+                    img: "assets/3.jpg",
+                    isFav: true
+                }
+            ]
         }
     },
     methods: {
       toggleShowBooks() {
         this.showBooks = !this.showBooks
       },
-      handleEvent(e, data) {
-        console.log(e);
-        if(data) {
-            console.log(data);
-        }
-      },
-      handleMouseMove(e) {
-        this.x = e.offsetX
-        this.y = e.offsetY
-      }
+
     }
 });
 
